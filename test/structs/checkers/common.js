d152d8e4f2648c2d7830fa22c14971aecd16413a
Javran Cheng <Javran.c@gmail.com>
2017-06-14 10:54:30 -0400

    comment tests

--- test/structs/checkers/common.js
+++ test/structs/checkers/common.js
@@ -6,11 +6,13 @@ const spec = it
 
 describe('CheckMethod', () => {
   spec('tests', () => {
+    // example: test(1, 'gt', 2, 1 >= 2)
     const test = (x, typ, y, expected) =>
       assert.equal(
         CheckMethod.toFunction({type: typ, value: y})(x),
         expected)
 
+    // just need a small matrix to expose the problem, if any.
     const batchTest = (typ, func) => {
       for (let x=0; x<5; ++x)
         for (let y=0; y<5; ++y)
